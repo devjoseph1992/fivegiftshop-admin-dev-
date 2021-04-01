4c48c29506587adc1d84e683c70e40783038071e
John Joseph Florina <devjosephflorina@gmail.com>
2021-04-02 00:19:19 +0800

    base of chakra firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
+import { AuthProvider } from '@/utils/auth';
+import { ChakraProvider } from '@chakra-ui/react';
+
 function MyApp({ Component, pageProps }) {
- return <Component {...pageProps} />;
+ return (
+  <ChakraProvider>
+   <AuthProvider>
+    <Component {...pageProps} />
+   </AuthProvider>
+  </ChakraProvider>
+ );
 }
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import { useAuth } from '@/utils/auth';
 
-const index = () => {
- return <div>hello</div>;
+// styles
+import { Button, Center, Text } from '@chakra-ui/react';
+import { FaGithub } from 'react-icons/fa';
+
+const Home = () => {
+ const auth = useAuth();
+ return (
+  <>
+   <div>{auth?.user?.email}</div>
+   <Center p={8}>
+    <Button
+     onClick={(e) => auth.signinWithGitHub()}
+     w={'full'}
+     maxW={'md'}
+     variant={'outline'}
+     leftIcon={<FaGithub />}
+    >
+     <Center>
+      <Text>Sign in with Github</Text>
+     </Center>
+    </Button>
+    {auth?.user && (
+     <Button
+      onClick={(e) => auth.signout()}
+      w={'full'}
+      maxW={'md'}
+      variant={'outline'}
+     >
+      <Center>
+       <Text>Logout</Text>
+      </Center>
+     </Button>
+    )}
+   </Center>
+  </>
+ );
 };
 
-export default index;
+export default Home;
